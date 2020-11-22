6743988caca76bb2ae5d9f8d35febe18f6c1e5d8
joprcamargo <58490789+joprcamargo@users.noreply.github.com>
2020-11-22 11:39:07 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'dieta/index'
   get 'dieta/' => "dieta#index"
   get 'dieta/:id' => "dieta#show"
+  root "calculadoras#index"
 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
