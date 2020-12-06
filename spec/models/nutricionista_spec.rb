0dbe83e67d194e61c472d7851bc9a9d50b7e358a
joprcamargo <58490789+joprcamargo@users.noreply.github.com>
2020-12-06 19:25:23 -0300

    testes nutricionista

--- spec/models/nutricionista_spec.rb
+++ spec/models/nutricionista_spec.rb
@@ -1,1 +1,41 @@
-require 'nutricionistum.rb'
+require 'rails_helper'
+
+    describe Nutricionistum do 
+    it "é válido quando nome, último nome e email estão presentes" do 
+     nutricionistum = Nutricionistum.new(nome: 'Bruce', 
+      id: '2', 
+      email: 'bruce@ironmaiden.com',
+      exp: '12')
+      expect(nutricionistum).to be_valid 
+     end 
+   end
+
+   describe Nutricionistum do 
+    it "é invalido sem o primeiro nome" do 
+     nutricionistum = Nutricionistum.new(nome: nil) 
+     nutricionistum.valid?   
+     expect(nutricionistum.errors[:nome]).to include("can't be blank") 
+    end 
+   end
+
+   describe Nutricionistum do 
+    it "é inválido caso já exista um e-mail igual" do 
+     nutricionistum =  Nutricionistum.create( nome: 'Steve', 
+      id: '12', email: 'contato@ironmaiden.com', exp: '12' ) 
+     nutricionistum = Nutricionistum.new( nome: 'Bruce', 
+      id: '1', email: 'contato@ironmaiden.com', exp: '12' ) 
+     nutricionistum.valid? 
+     expect(nutricionistum.errors[:email]).to include('has already been taken')
+    end 
+   end
+
+   describe Nutricionistum do 
+    it "é inválido caso já exista um CRN igual" do 
+     nutricionistum =  Nutricionistum.create( nome: 'Steve', 
+      id: '1', email: 'contato@ironmaiden.com', exp: '12' ) 
+     nutricionistum = Nutricionistum.new( nome: 'Bruce', 
+      id: '1', email: 'contato2@ironmaiden.com', exp: '12' ) 
+     nutricionistum.valid? 
+     expect(nutricionistum.errors[:id]).to include('has already been taken')
+    end 
+   end
