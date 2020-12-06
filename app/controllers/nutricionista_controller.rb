9f054e9ff81cc3fe6cb53966eee73a67613d2b2a
joprcamargo <58490789+joprcamargo@users.noreply.github.com>
2020-12-06 11:57:24 -0300

    teste submit form

--- app/controllers/nutricionista_controller.rb
+++ app/controllers/nutricionista_controller.rb
@@ -6,4 +6,23 @@ class NutricionistaController < ApplicationController
  
  def index 
  end
+
+ def show
+    @nutricionista = Nutricionistum.find(params[:id])
+ end
+
+ def create
+    @nutricionista = Nutricionistum.new(nutricionista_params)
+    if @nutricionista.save
+      redirect_to @nutricionista
+    else
+      render "new"
+    end
+end
+
+
+        private def nutricionista_params
+            params.require(:nutricionista).permit(:nome, :id, :email, :exp)
+        end   
+
 end
